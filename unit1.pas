7e287aa442ccc9986ccba8c0bb8b735429bc50e2
Stephane B <sirber@hotmail.com>
2015-06-09 12:03:56 -0400

    initial

---
+++ unit1.pas
@@ -0,0 +1,472 @@
+unit Unit1; 
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, IBConnection, FileUtil, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, ComCtrls, AsyncProcess, Menus;
+
+type
+
+  { TForm1 }
+  TForm1 = class(TForm)
+    cboVPreset: TComboBox;
+    cboVProfile: TComboBox;
+    cboVTune: TComboBox;
+    cboVDenoise: TComboBox;
+    chkDXVA2: TCheckBox;
+    chkOpenCL: TCheckBox;
+    chkFResize: TCheckBox;
+    chkFDeblock: TCheckBox;
+    chkFDenoise: TCheckBox;
+    chkSBurn: TCheckBox;
+    GroupBox4: TGroupBox;
+    Label12: TLabel;
+    txtFHResize: TEdit;
+    txtFWResize: TEdit;
+    GroupBox3: TGroupBox;
+    Label10: TLabel;
+    Label11: TLabel;
+    mmoHelp: TMemo;
+    Subtitle: TGroupBox;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    Label7: TLabel;
+    Label8: TLabel;
+    Label9: TLabel;
+    Help: TTabSheet;
+    txtATrack: TEdit;
+    txtSTrack: TEdit;
+    txtVBitrate: TEdit;
+    Label3: TLabel;
+    oProcess: TAsyncProcess;
+    cmdStart: TButton;
+    cmdStop: TButton;
+    Button3: TButton;
+    Audio: TGroupBox;
+    cboVCodec: TComboBox;
+    cboACodec: TComboBox;
+    Label1: TLabel;
+    Label2: TLabel;
+    txtABitrate: TEdit;
+    Video: TGroupBox;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    pmFiles: TPopupMenu;
+    txtDestination: TEdit;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    lstLogs: TListBox;
+    lstFiles: TListBox;
+    memoAbout: TMemo;
+    PageControl1: TPageControl;
+    StatusBar1: TStatusBar;
+    Status: TTabSheet;
+    Files: TTabSheet;
+    Settings: TTabSheet;
+    About: TTabSheet;
+    procedure cboACodecChange(Sender: TObject);
+    procedure cboVCodecChange(Sender: TObject);
+    procedure chkFResizeChange(Sender: TObject);
+    procedure chkFDenoiseChange(Sender: TObject);
+    procedure cmdStartClick(Sender: TObject);
+    procedure cmdStopClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+    procedure FormDropFiles(Sender: TObject; const FileNames: array of String);
+    procedure MenuItem1Click(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+    procedure oProcessReadData(Sender: TObject);
+    procedure oProcessTerminate(Sender: TObject);
+  private
+    { private declarations }
+    procedure encode_start(sFile: string; sCmdLine: string);
+    procedure addLog(sMessage: string);
+    procedure updateStatus(sStatus: string);
+    function makeOutput(sFile: string):string;
+  public
+    { public declarations }
+  end; 
+
+var
+  Form1: TForm1;
+
+const
+  sVersion: string = '2014-10-05 x64 dev';
+
+implementation
+
+{$R *.lfm}
+
+{ Misc functions }
+function CountPos(const subtext: string; Text: string): Integer;
+begin
+  if (Length(subtext) = 0) or (Length(Text) = 0) or (Pos(subtext, Text) = 0) then
+    Result := 0
+  else
+    Result := (Length(Text) - Length(StringReplace(Text, subtext, '', [rfReplaceAll]))) div Length(subtext);
+end;
+
+{ TForm1 }
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  Form1.Text:='Recode v' + sVersion;
+  Application.Title:=Form1.Text;
+  addLog(Form1.Text);
+  memoAbout.Lines.Text := StringReplace(memoAbout.Lines.Text, '{version}', sVersion, [rfIgnoreCase]);   ;
+
+  PageControl1.TabIndex:=0;
+end;
+
+{ Status }
+procedure TForm1.cmdStartClick(Sender: TObject);
+var
+  sFile: string;
+  sACodec: string;
+  iEncoder: integer;
+  bPP: boolean;
+  sCli: string;
+  sCmdLine: string;
+begin
+  if (lstFiles.Items.Count = 0) then
+  begin
+    cmdStopClick(Sender);
+    updateStatus('done');
+    Exit;
+  end;
+  bPP := false;
+
+  cmdStop.Enabled:=true;
+  cmdStart.Enabled:=false;
+
+  { make cmdline }
+  sFile := lstFiles.Items.Strings[0];
+
+  { detect encoder to use }
+  iEncoder := 0; // handbrake
+
+  { pass control }
+  case iEncoder of
+    0: // handbrake
+    begin
+      { encoder }
+      sCmdLine := '"' + ExtractFilePath(Application.ExeName) + 'bin/HandBrakeCLI.exe" ';
+
+      { input / output }
+      sCmdLine := sCmdLine + '-i "' + sFile + '" ';
+
+      {video}
+      case cboVCodec.ItemIndex of
+        0: // x264
+        begin
+          sCmdLine := sCmdLine + '-e x264 ';
+          sCmdLine := sCmdLine + '--encoder-preset ' + cboVPreset.Items.Strings[cboVPreset.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '--encoder-tune ' + cboVTune.Items.Strings[cboVTune.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '--encoder-profile ' + cboVProfile.Items.Strings[cboVProfile.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '-2 -T '; // 2-pass, Turbo first pass
+        end;
+        1: // theora
+        begin
+          sCmdLine := sCmdLine + '-e theora ';
+        end;
+        2: // x265
+        begin
+          sCmdLine := sCmdLine + '-e x265 ';
+          sCmdLine := sCmdLine + '--encoder-preset ' + cboVPreset.Items.Strings[cboVPreset.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '--encoder-tune ' + cboVTune.Items.Strings[cboVTune.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '--encoder-profile ' + cboVProfile.Items.Strings[cboVProfile.ItemIndex] + ' ';
+          sCmdLine := sCmdLine + '-2 -T '; // 2-pass, Turbo first pass
+        end;
+        3: // vp8
+        begin
+          sCmdLine := sCmdLine + '-e vp8 ';
+        end;
+        4: // mpeg4
+        begin
+          sCmdLine := sCmdLine + '-e mpeg4 ';
+        end;
+      end;
+
+      { settings / codec / common}
+      sCmdLine := sCmdLine + '-b ' + txtVBitrate.Text + ' ';
+
+      { advanced }
+      if (chkOpenCL.Checked) then sCmdLine := sCmdLine + '--use-opencl ';
+      if (chkDXVA2.Checked) then sCmdLine := sCmdLine + '--use-hwd ';
+
+      {output}
+      sCmdLine := sCmdLine + '-o "' + makeOutput(sFile) + '" ';
+
+      { audio }
+      case cboACodec.ItemIndex of
+        0: sACodec := 'vorbis'; // vorbis
+        1: sACodec := 'ffaac'; // aac
+        2: sACodec := 'lame'; // mp3
+      end;
+      sCmdLine := sCmdLine + '-E ' + sACodec + ' ';;
+      sCmdLine := sCmdLine + '-a ' + txtATrack.Text + ' ';
+      sCmdLine := sCmdLine + '-B ' + txtABitrate.Text + ' ';
+
+      { subtitle }
+      if (strlen(pchar(txtSTrack.Text)) > 0) then
+      begin
+        sCmdLine := sCmdLine + '-s ' + txtSTrack.Text + ' ';
+        if (chkSBurn.Checked) then
+           sCmdLine := sCmdLine + '--subtitle-burn ';
+      end;
+
+      { filtering }
+      if (chkFDenoise.Checked) then
+        sCmdLine := sCmdLine + '--denoise ' + cboVDenoise.Items.Strings[cboVDenoise.ItemIndex] + ' ';
+      if (chkFDeblock.Checked) then
+        sCmdLine := sCmdLine + '--deblock ';
+      if (chkFResize.Checked) then
+      begin
+        if (Length(txtFWResize.Text) > 0) then
+          sCmdLine := sCmdLine + '--width ' + txtFWResize.Text + ' ';
+        if (Length(txtFHResize.Text) > 0) then
+          sCmdLine := sCmdLine + '--height ' + txtFHResize.Text + ' ';
+      end;
+    end;
+
+    1: // ffmpeg2theora
+    begin
+      { encoder }
+      sCmdLine := '"' + ExtractFilePath(Application.ExeName) + 'bin/ffmpeg2theora/ffmpeg2theora.exe" ';
+      { output }
+      sCmdLine := sCmdLine + '-o "' + makeOutput(sFile) + '" ';
+      { setings video}
+      sCmdLine := sCmdLine + '-V ' + txtVBitrate.Text + ' ';
+      sCmdLine := sCmdLine + '--soft-target --two-pass --optimize ';
+      { audio }
+      sCmdLine := sCmdLine + '-A ' + txtABitrate.Text + ' ';
+
+      { filtering }
+      if (chkFDenoise.Checked) then
+      begin
+        if (bPP = false) then
+        begin
+          sCmdLine := sCmdLine + '--pp "';
+          bPP:= true;
+        end;
+        sCmdLine := sCmdLine + 'tn:64:128:256';
+      end;
+      if (chkFDeblock.Checked) then
+      begin
+        if (bPP = false) then
+        begin
+          sCmdLine := sCmdLine + '--pp "';
+          bPP:= true;
+        end
+        else
+          sCmdLine := sCmdLine + ',';
+        sCmdLine := sCmdLine + 'vb:a,hb:a';
+      end;
+      if (bPP) then
+        sCmdLine := sCmdLine + '"';
+      sCmdLine := sCmdLine + ' ';
+
+      if (chkFResize.Checked) then
+      begin
+        if (Length(txtFWResize.Text) > 0) then
+          sCmdLine := sCmdLine + '--width ' + txtFWResize.Text + ' ';
+        if (Length(txtFHResize.Text) > 0) then
+          sCmdLine := sCmdLine + '--height ' + txtFHResize.Text + ' ';
+      end;
+
+      { input }
+      sCmdLine := sCmdLine + ' "' + sFile + '" ';
+    end;
+  end;
+
+  { Fix}
+  sCmdLine := StringReplace(sCmdLine, '\\', '\', [rfIgnoreCase]);
+
+  mmoHelp.Append(sCmdLine);
+
+  { start }
+  encode_start(sFile, sCmdLine);
+end;
+
+function TForm1.makeOutput(sFile: string):string;
+var
+  bFound: boolean;
+  sOutput: string;
+  sSuffix: string;
+  iCpt: integer;
+begin
+  sSuffix := '';
+  iCpt := 0;
+  repeat
+    bFound := false;
+    sOutput := txtDestination.Text + ExtractFileNameOnly(sFile) + sSuffix;
+    sOutput := sOutput + '.mkv';
+    {case cboPreset.ItemIndex of
+      0: sOutput := sOutput + '.mkv';
+      1: sOutput := sOutput + '.mp4';
+      2: sOutput := sOutput + '.mp4';
+    end;}
+    sOutput := StringReplace(sOutput, '{source}', ExtractFilePath(sFile), [rfIgnoreCase]);
+
+    if (FileExists(sOutput)) then
+    begin
+      // File exists, add suffix
+      inc(iCpt);
+      sSuffix := '('+IntToStr(iCpt)+')';
+      bFound:=true;
+    end;
+  until bFound = false;
+
+  Result:= sOutput;
+end;
+
+procedure TForm1.cmdStopClick(Sender: TObject);
+begin
+  oProcess.Terminate(1);
+  cmdStop.Enabled:=false;
+  cmdStart.Enabled:=true;
+  updateStatus('aborted');
+end;
+
+procedure TForm1.addLog(sMessage: string);
+var
+  sDate: string;
+begin
+  sDate := '[' + TimeToStr(time()) + '] ';
+  lstLogs.Items.Add(sDate + sMessage);
+  lstLogs.ItemIndex := lstLogs.Items.Count - 1;
+end;
+
+procedure TForm1.updateStatus(sStatus: string);
+begin
+  StatusBar1.Panels.Items[1].Text := sStatus;
+end;
+
+{ Files }
+procedure TForm1.FormDropFiles(Sender: TObject; const FileNames: array of String);
+var
+  iCpt: integer;
+begin
+  for iCpt := 0 to (Length(FileNames) - 1) do
+  begin
+    if (FileExistsUTF8(FileNames[iCpt])) then
+        lstFiles.Items.Add(FileNames[iCpt]);
+  end;
+  PageControl1.TabIndex:=1;
+end;
+
+procedure TForm1.MenuItem1Click(Sender: TObject);
+begin
+  if ((oProcess.Active) and (lstFiles.ItemIndex = 0)) then
+    ShowMessage('Cannot remove this file while encoding.')
+  else
+    lstFiles.Items.Delete(lstFiles.ItemIndex);
+end;
+
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+  if (oProcess.Active) then
+  begin
+    while (lstFiles.Items.Count > 1) do // remove all files except the first one
+      lstFiles.Items.Delete(1);
+  end
+  else
+    lstFiles.Clear;
+end;
+
+{ Settings }
+procedure TForm1.cboVCodecChange(Sender: TObject);
+begin
+  // x264 specific
+  case cboVCodec.ItemIndex of
+    0,2: // x264, x265
+    begin
+      cboVPreset.Enabled := true;
+      cboVTune.Enabled := true;
+      cboVProfile.Enabled := true;
+    end;
+    1,3: // Theora, vp8
+    begin
+      cboVPreset.Enabled := false;
+      cboVTune.Enabled := false;
+      cboVProfile.Enabled := false;
+    end;
+  end;
+end;
+
+procedure TForm1.chkFResizeChange(Sender: TObject);
+begin
+  txtFWResize.Enabled:=false;
+  txtFWResize.Text:='';
+  txtFHResize.Enabled:=false;
+  txtFhResize.Text:='';
+  if (chkFResize.Checked) then
+  begin
+    txtFWResize.Enabled:=true;
+    txtFHResize.Enabled:=true;
+  end;
+end;
+
+procedure TForm1.chkFDenoiseChange(Sender: TObject);
+begin
+  if ((chkFDenoise.Checked) and (cboVCodec.ItemIndex = 0)) then
+    cboVDenoise.Enabled:=true
+  else
+    cboVDenoise.Enabled:=false;
+end;
+
+procedure TForm1.cboACodecChange(Sender: TObject);
+begin
+  case cboACodec.ItemIndex of
+    0: txtABitrate.Text:='64';
+    1: txtABitrate.Text:='64';
+    2: txtABitrate.Text:='96';
+  end;
+  txtATrack.Text := '1';
+end;
+
+{ About }
+
+{*** ENCODING ***}
+procedure TForm1.encode_start(sFile: string; sCmdLine: string);
+begin
+  addLog('Encoding: ' + ExtractFileNameOnly(sFile));
+  oProcess.CommandLine:=sCmdLine;
+  oProcess.Execute;
+end;
+
+procedure TForm1.oProcessTerminate(Sender: TObject);
+begin
+  if (oProcess.ExitStatus <> 0) then
+  begin
+    addLog('> error.');
+    cmdStopClick(Sender);
+    Exit;
+  end;
+
+  addLog('> finished.');
+
+  // Remove encoded file
+  lstFiles.Items.Delete(0);
+
+  // Start again
+  cmdStartClick(Sender);
+end;
+
+procedure TForm1.oProcessReadData(Sender: TObject);
+var
+  aOutput: TStringList;
+begin
+  aOutput := TStringList.Create();
+  aOutput.LoadFromStream(oProcess.Output);
+  if (aOutput.Count > 0) then
+  begin
+    updateStatus(aOutput.Strings[aOutput.Count - 1]);
+  end;
+end;
+
+
+
+end.
